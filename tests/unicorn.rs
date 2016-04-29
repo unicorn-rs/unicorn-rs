1b0c659e3c1798bf2e07d93348eaa7c5a83a77a6
petevine <tulipawn@gmail.com>
2016-04-29 07:09:20 +0200

    Add ARM Thumb example

--- tests/unicorn.rs
+++ tests/unicorn.rs
@@ -1,6 +1,6 @@
 extern crate unicorn;
 
-use unicorn::{Cpu, CpuX86, CpuMIPS, uc_handle};
+use unicorn::{Cpu, CpuX86, CpuARM, CpuMIPS, uc_handle};
 
 #[test]
 fn emulate_x86() {
@@ -25,6 +25,8 @@ fn emulate_x86() {
     assert_eq!(emu.reg_read(unicorn::RegisterX86::EDX), Ok((49)));
 }
 
+
+
 #[test]
 fn emulate_x86_negative_values() {
     let x86_code32 : Vec<u8> = vec![0x41, 0x4a]; // INC ecx; DEC edx
@@ -41,7 +43,6 @@ fn emulate_x86_negative_values() {
     assert_eq!(emu.reg_read_i32(unicorn::RegisterX86::ECX), Ok((-9)));
     assert_eq!(emu.reg_read_i32(unicorn::RegisterX86::EDX), Ok((-51)));
 }
-
 
 #[test]
 fn x86_code_callback() {
@@ -79,6 +80,29 @@ fn x86_mem_callback() {
     assert_eq!(emu.reg_write(unicorn::RegisterX86::EAX, 0x123), Ok(()));
     assert_eq!(emu.emu_start(0x1000, 0x1001, 10 * unicorn::SECOND_SCALE, 1), Err((unicorn::Error::READ_UNMAPPED)));
     assert_eq!(emu.remove_hook(hook), Ok(()));
+}
+
+#[test]
+fn emulate_arm() {
+    let arm_code32 : Vec<u8> = vec![0x83, 0xb0]; // sub    sp, #0xc 
+
+    let mut emu = CpuARM::new(unicorn::Mode::THUMB).expect("failed to instantiate emulator");
+    assert_eq!(emu.reg_write(unicorn::RegisterARM::R1, 123), Ok(()));
+    assert_eq!(emu.reg_read(unicorn::RegisterARM::R1), Ok((123)));
+
+    // Attempt to write to memory before mapping it.
+    assert_eq!(emu.mem_write(0x1000, &arm_code32), (Err(unicorn::Error::WRITE_UNMAPPED)));
+
+    assert_eq!(emu.mem_map(0x1000, 0x4000, unicorn::PROT_ALL), Ok(()));
+    assert_eq!(emu.mem_write(0x1000, &arm_code32), Ok(()));
+    assert_eq!(emu.mem_read(0x1000, arm_code32.len()), Ok(arm_code32.clone()));
+
+    assert_eq!(emu.reg_write(unicorn::RegisterARM::SP, 12), Ok(()));
+    assert_eq!(emu.reg_write(unicorn::RegisterARM::R0, 10), Ok(()));
+
+    assert_eq!(emu.emu_start(0x1000, (0x1000 + arm_code32.len()) as u64, 10 * unicorn::SECOND_SCALE, 1000), Ok(()));
+    assert_eq!(emu.reg_read(unicorn::RegisterARM::SP), Ok((0)));
+    assert_eq!(emu.reg_read(unicorn::RegisterARM::R0), Ok((10)));
 }
 
 #[test] 
