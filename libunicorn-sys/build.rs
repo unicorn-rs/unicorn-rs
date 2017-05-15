7619b35eafeb796a0d6bac051a4f98a5c1dc129a
Sébastien Duquette <ekse.0x@gmail.com>
2017-05-15 18:52:46 -0400

    Use build-helper crate.

--- libunicorn-sys/build.rs
+++ libunicorn-sys/build.rs
@@ -1,3 +1,4 @@
+extern crate build_helper;
 extern crate gcc;
 extern crate os_type;
 extern crate pkg_config;
@@ -5,14 +6,11 @@ extern crate pkg_config;
 use std::path::Path;
 use std::process::Command;
 use std::env;
-use std::io::ErrorKind;
-use std::fs;
+
+use build_helper::rustc::{link_search, link_lib};
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    let windows = target.contains("windows");
-
-    if !target.contains("windows") {
+    if !build_helper::windows() {
         if Command::new("pkg-config").output().is_ok()
             && pkg_config::Config::new().atleast_version("1.0.0").probe("unicorn").is_ok() {
             return;
@@ -31,27 +29,19 @@ fn main() {
         _ => [""],
     };
 
-    // TODO(sduquette): the package build should fail if this command fails.
-    if !target.contains("msvc") {
-        let _ = Command::new("./make.sh").args(&make_args).current_dir("unicorn").status();
-
-        let unicorn = "libunicorn.a";
-        let _ = Command::new("cp").current_dir("unicorn").arg(&unicorn).arg(&out_dir).status();
-        
-        println!("cargo:rustc-link-search=native={}", out_dir);
-        println!("cargo:rustc-link-lib=static=unicorn");
-    } else {
+    if build_helper::target::triple().env().unwrap_or("").contains("msvc") {
         let libpath = env::var("LIB").unwrap();
         env::set_var("LIB", libpath + ";" + &out_dir);
         
-        let platformToolSet = match env::var("PLATFORMTOOLSET") {
+        let platform_toolset = match env::var("PLATFORMTOOLSET") {
             Ok(x) => format!("PlatformToolset={};", x),
             Err(_) => "".to_owned(),
         };
 
-        let properties = "/p:OutDir=".to_owned() + &out_dir + "/;" + &platformToolSet + "useenv=true;Configuration=Release";
+        let properties = "/p:OutDir=".to_owned() + &out_dir + "/;" + &platform_toolset
+                         + "useenv=true;Configuration=Release";
         let status = match Command::new("msbuild")
-                            .args(&["msvc/unicorn.sln", &properties])
+                            .args(&["msvc/unicorn.sln", "/t:unicorn_static", &properties])
                             .current_dir("unicorn").status() {
             Ok(status) => status,
             Err(e) => fail(&format!("failed to execute command: {}", e)),
@@ -60,9 +50,18 @@ fn main() {
             fail(&format!("command did not execute successfully, got {}", status));
         }
 
-        println!("cargo:rustc-link-search=native={}", &out_dir);
-        println!("cargo:rustc-link-lib=static=unicorn_static");
-    }    
+        link_search(Some(build_helper::SearchKind::Native), build_helper::out_dir());
+        link_lib(Some(build_helper::LibKind::Static), "unicorn_static");
+    } else {
+        // TODO(sduquette): the package build should fail if this command fails.
+        let _ = Command::new("./make.sh").args(&make_args).current_dir("unicorn").status();
+
+        let unicorn = "libunicorn.a";
+        let _ = Command::new("cp").current_dir("unicorn").arg(&unicorn).arg(&out_dir).status();
+        
+        link_search(Some(build_helper::SearchKind::Native), build_helper::out_dir());
+        link_lib(Some(build_helper::LibKind::Static), "unicorn");
+    }
 }
 
 fn fail(s: &str) -> ! {
